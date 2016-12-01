b84b4c74c2c70a52b6cc5a286408d57e9164d6d5
Bryan Campuzano <bryan.campuzano82@gmail.com>
2016-12-01 10:46:46 -0800

    trying to get it to work

--- game.js
+++ game.js
@@ -21,10 +21,15 @@ document.addEventListener("keydown", function(e) {
  document.getElementById("bat").setAttribute("y", BatY + 15)
  BatY = BatY + 15;
   }
+
+  var food1x = Number(document.getElementById("food1").getAttribute("x"))
+   var food1y = Number(document.getElementById("food1").getAttribute("y"))
+   var food2x = Number(document.getElementById("food2").getAttribute("x"))
+   var food2y = Number(document.getElementById("food2").getAttribute("y"))
+
+
+  if (BatX > food1x && BatY < food1y + 20 && BatY > food1y && BatY < food1y + 20) {
+    console.log("Overlap!!")
+  }
+
 })
-
-
-var food1X=Number(document.getElementById("food1").getAttribute("x"));
- var food1y=Number(document.getElementById("food1").getAttribute("y"));
- var food2X=Number(document.getElementById("food2").getAttribute("x"));
- var food2y=Number(document.getElementById("food2").getAttribute("y"));
